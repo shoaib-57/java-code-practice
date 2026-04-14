2bc74925d565c86e0ad50ebcc00d3d40700dcbb3
shoaib-57 <workshoaib6797@gmail.com>
2026-04-14 23:42:09 +0530

    daily upload

---
+++ Arithmetic.java
@@ -0,0 +1,12 @@
+public class Arithmetic {
+    try {
+        int result  = 10/0;
+    } catch (ArithmeticException e ) {
+        System.out.println("cannot divide by zero");
+
+    }finally {
+        System.out.println("this block will always execute ");
+    }
+}
+
+ 
