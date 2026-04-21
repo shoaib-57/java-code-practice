dbdbe95b2caae076135ece09ea1cd358bfeaf6e2
shoaib-57 <workshoaib6797@gmail.com>
2026-04-21 19:34:23 +0530

    daily upload

--- Hashmap.java
+++ Hashmap.java
@@ -69,7 +69,7 @@ public class Hashmap {
 
         }
        for (String name : map.keySet()){
-        System.out.println(name + " : " + map.get(name));12
+        System.out.println(name + " : " + map.get(name));1
        }
     }
 }
