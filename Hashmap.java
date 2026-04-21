6c6c2a052a8d714b71fc37af1665e3ba63a5716d
shoaib-57 <workshoaib6797@gmail.com>
2026-04-21 19:33:34 +0530

    daily upload

--- Hashmap.java
+++ Hashmap.java
@@ -1,8 +1,8 @@
 import java.util.HashMap;
 
-public class Hashmap {
-    public static void main(String[] args) {
-        HashMap<String , Integer > map = new HashMap <String , Integer>();
+// public class Hashmap {
+//     public static void main(String[] args) {
+//         HashMap<String , Integer > map = new HashMap <String , Integer>();
 
         // map.put("Alice ", 99);
         // map.get("Alice");
@@ -13,18 +13,63 @@ public class Hashmap {
         // System.out.println();
 
 
-        map.put("Alice",90);
-        map.put("Bob", 75);
-        map.put("Charlie",85);
+//         map.put("Alice",90);
+//         map.put("Bob", 75);
+//         map.put("Charlie",85);
 
-        for (int i = 0; i<map.; i++){
+//         for (String name : map.keySet()){
+//         System.out.println(name + " : " + map.get(name ));
+//         }
+//         //for total students 
+//         map.put("Bob", 95);
+//         System.out.println(" Bobs updated marks : " + map.get("Bob") );
+
+//        //total students
+//        System.out.println("total number of students : " + map.size());
+       
+
+
+//     }
+// }
+// public class Hashmap{
+//     public static void main(String[] args) {
+//          HashMap<String , Integer > map = new HashMap <String , Integer>();
+
+//          map.put("Alice",90);
+//          map.put("Bob", 75);
+//          map.put("Charlie",85);
+
+        
+//         if (map.containsKey("Alice")){
+//             System.out.println("Alice found ");
+//         }
+//         map.remove("Charlie");
+
+//         System.out.println("Remaining students ");
+
+//          for (String name : map.keySet()){
+// //         
+        
+//         System.out.println(name + " : " + map.get(name ));
+//     }
+// }
+// }
+public class Hashmap {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap <String , Integer>();
+
+        String[] words = {"apple", "banana", "apple", "cherry", "banana", "apple"};
+        
+        for(String word : words  )
+        if (map.containsKey(word)){
+            map.put(word , map.get(word) + 1);
+        
+        }else {
+            map.put(word , 1);
 
         }
-
-        //for total students 
-        for (int i =0; i<    ; i++){
-          
-        }
-
+       for (String name : map.keySet()){
+        System.out.println(name + " : " + map.get(name));
+       }
     }
 }
