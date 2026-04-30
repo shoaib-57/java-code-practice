2d3e174f3bb41fcc784e7fab40cd30d37203a55e
shoaib-57 <workshoaib6797@gmail.com>
2026-04-30 19:25:40 +0530

    daily upload

--- myThread.java
+++ myThread.java
@@ -44,5 +44,22 @@ class Thread2 extends Thread {
     }
 }
 
+class MyTask implements Runnable{
+    public void run(){
+        for (int i=1; i<=3; i++){
+            System.out.println("Task is running!");
+
+        }
+    }
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread t = new Thread(task);
+
+        t.start();
+        
+    }
+
+}
+
 
 
